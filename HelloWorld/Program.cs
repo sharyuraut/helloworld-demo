d9edf7a52d605e62bf312e4f60f4c14039c6cb35
sharyu raut <sharyukraut210298@gmail.com>
2020-09-24 10:32:58 +0530

    [Sharyu Raut] Add.commit first

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿//using HelloWorld.services;
+using HelloWorld.services;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Calculation cal = new Calculation();
+            int c = cal.AddNumber(5, 10);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/services/Calculation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.services
+{
+    public class Calculation
+    {
+        public int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
